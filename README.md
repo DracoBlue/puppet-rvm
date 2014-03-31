6cfda17e0dd36d7c3f706a7f8290cabc2a9d5cc2
DracoBlue <JanS@DracoBlue.de>
2014-03-31 09:41:08 +0200

    Fixed cwd in bash_exec. fixes #1

--- README.md
+++ README.md
@@ -119,6 +119,7 @@ eascaping on quotation marks. So if you want to run a command only under certian
 # Changelog
 
 * dev
+  - fixed cwd in `rvm::bash_exec` #1
   - renamed `rvm::bash-exec` to `rvm::bash_exec`
 * 0.1.0 (2014/03/30)
   - Initial release
--- manifests/bash_exec.pp
+++ manifests/bash_exec.pp
@@ -1,4 +1,4 @@
-define rvm::bash-exec (
+define rvm::bash_exec (
   $command = $name,
   $user,
   $creates = undef,
@@ -18,24 +18,31 @@ define rvm::bash-exec (
   $umask = undef,
   $unless = undef
 ) {
+  if $cwd == undef {
+    $command_prefix = "/bin/su -l ${user} -c \"/bin/bash --login -c \\\""
+  } else {
+    $command_prefix = "/bin/su -l ${user} -c \"/bin/bash --login -c \\\"cd ${cwd} && "
+  }
+  $command_suffix = "\\\"\""
+
   $escaped_command = regsubst($command, "\"", "\\\"", 'G')
 
   if $unless == undef {
     $escaped_unless = undef
   } else {
     $unless_with_escaped_quotes = regsubst($unless, "\"", "\\\"", 'G')
-    $escaped_unless = "/bin/su -l ${user} -c \"/bin/bash --login -c \\\"${unless_with_escaped_quotes}\\\"\""
+    $escaped_unless = "${$command_prefix}${unless_with_escaped_quotes}${command_suffix}"
   }
 
   if $onlyif == undef {
     $escaped_onlyif = undef
   } else {
     $onlyif_with_escaped_quotes = regsubst($onlyif, "\"", "\\\"", 'G')
-    $escaped_onlyif = "/bin/su -l ${user} -c \"/bin/bash --login -c \\\"${onlyif_with_escaped_quotes}\\\"\""
+    $escaped_onlyif = "${$command_prefix}${onlyif_with_escaped_quotes}${command_suffix}"
   }
 
   exec { $name:
-    command => "/bin/su -l ${user} -c \"/bin/bash --login -c \\\"${escaped_command}\\\"\"",
+    command => "${$command_prefix}${escaped_command}${command_suffix}",
     creates => $creates,
     cwd => $cwd,
     environment => $environment,
